a318470c8c50a266bd7978c99c93ffc2b2d0d821
hieuhunter <levanhieu12a7@gmail.com>
2022-06-19 17:01:16 +0700

    up

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -8,11 +8,11 @@ export default function Document() {
 				<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 				<meta httpEquiv='X-UA-Compatible' content='ie=edge' />
 				{/* <titles>World Time</titles> */}
-				<link rel='shortcut icon' href='assets/images/favicon.png' />
+				<link rel='shortcut icon' href='assets/images/iconH.png' />
 				<link rel='stylesheets' href='./assets/vendors/mdi/css/materialdesignicons.min.css' />
 				<link rel='stylesheets' href='assets/vendors/aos/dist/aos.css/aos.css' />
 				<link rel='stylesheets' href='assets/css/style.css' />
-				
+
 			</Head>
 			<body>
 				<Main />
